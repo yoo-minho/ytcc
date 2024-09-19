aa615cd19923fb1d8fde4efce70f809ad8f9c196
yoominho <wingedcompany@gmail.com>
2024-09-20 00:36:44 +0900

    ㅂㅈㄷ

--- server/api/trend/index.ts
+++ server/api/trend/index.ts
@@ -1,10 +1,8 @@
 import { youtube } from "@/server/lib/youtubeDataApi";
 import { TrendingVideoType } from "@/types/comm";
 import dayjs from "dayjs";
-import relativeTime from "dayjs/plugin/relativeTime";
 import "dayjs/locale/ko";
 
-dayjs.extend(relativeTime);
 dayjs.locale("ko");
 
 export default defineEventHandler(async (event) => {
